8eb40af84b5157a04196bd857c47489ae165c64a
BT_4N2_21 <jackn25@nycstudents.net>
2023-09-19 13:39:00 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.lang.Math;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        //Ask for bill
+        System.out.print("Enter the total bill: ");
+        String totalBillPlaceholder = s.nextLine();
+        double totalBill = Double.parseDouble(totalBillPlaceholder);
+
+        //Ask for tip percent
+        System.out.print("Enter the percent tip(only whole numbers without % sign): ");
+        String tipPercentPlaceholder = s.nextLine();
+        double tipPercent = Double.parseDouble(tipPercentPlaceholder);
+
+        //Ask for amount of people
+        System.out.print("Enter the amount of people: ");
+        String numOfPeoplePlaceholder = s.nextLine();
+        int numOfPeople = Integer.parseInt(numOfPeoplePlaceholder);
+
+        //calculations
+        double tipAmount = totalBill * (tipPercent / 100);
+        double totalBillWithTip = tipAmount + totalBill;
+        double tipPerPerson = tipAmount / numOfPeople;
+        double totalPerPerson = totalBillWithTip / numOfPeople;
+
+        //Output
+        System.out.println("The amount you need to tip for the meal is " + tipAmount);
+        System.out.println("The total cost of the bill is " + totalBillWithTip);
+        System.out.println("The amount of tip per person is " + tipPerPerson);
+        System.out.println("The total amount per person is " + totalPerPerson);
+    }
+}
